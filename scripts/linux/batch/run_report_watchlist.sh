273fc1410e25b01148ef37cdc35cdc6254012bdc
Hyungsoo <minandsoo44@gmail.com>
2025-10-05 01:56:01 +0900

    change files

--- scripts/linux/batch/run_report_watchlist.sh
+++ scripts/linux/batch/run_report_watchlist.sh
@@ -8,5 +8,6 @@ PYTHONBIN="${PYTHONBIN:-python3}"
 bash scripts/linux/jobs/run_with_lock.sh \
   scripts/linux/jobs/_run_generic.sh \
     --log watchlist \
+    --guard th \
     -- \
     "$PYTHONBIN" report_watchlist_cli.py --date auto
